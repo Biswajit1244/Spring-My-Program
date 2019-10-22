ee795a1aabff711eb5d517cb3d5005f33a22caa6
Biswajit1244 <bbeuria9861@gmail.com>
2019-10-22 23:33:10 +0530

    ClassPathResource and getBean(_,_) changes

--- IOCProj-2_Constructor_Injection/src/com/biss/test/ConstructorInjectionTest.java
+++ IOCProj-2_Constructor_Injection/src/com/biss/test/ConstructorInjectionTest.java
@@ -2,6 +2,7 @@ package com.biss.test;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 import com.biss.beans.WishMessage;
@@ -13,13 +14,25 @@ public class ConstructorInjectionTest {
 		WishMessage msg=null;
 		
 		//create IOC container
-		factory=new XmlBeanFactory
-				(new FileSystemResource("src/com/biss/cfgs/applicationContext.xml"));
+		//factory=new XmlBeanFactory
+				//(new FileSystemResource("src/com/biss/cfgs/applicationContext.xml"));
 		
-		//get beans
-		msg=(WishMessage)factory.getBean("wmg");
+		//OR
 		
-		//call bussiness method
+		//create IOC container
+		factory =new XmlBeanFactory
+				(new ClassPathResource("com/biss/cfgs/applicationContext.xml"));
+		
+//		//get beans
+//		msg=(WishMessage)factory.getBean("wmg");
+//		
+//		//call bussiness method
+//		String message=msg.wishMessageGenerator("Biswa");
+		
+		//OR(by using new signature of getBean()
+		
+		//get spring bean obj
+		msg=factory.getBean("wmg",WishMessage.class);
 		String message=msg.wishMessageGenerator("Biswa");
 		System.out.println("Result:-"+message);
 	}
